cfbab7dd82b92ea11b364f6dcba8414831e801dc
vincent <vincent@Vincent>
2024-09-08 00:17:12 +0800

    Second version of code that refactor the code in main into lib.rs, opts.rs and process.rs

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,16 @@
 use clap::Parser;
 // rcli csv --input input.csv --output output.json --header -d '.'
-use csv::Reader;
-use rcli::{Opts, SubCommand};
-use serde::{Deserialize, Serialize};
 use anyhow;
-use serde_json;
-use std::fs::File;
+use rcli::{process_csv, Opts, SubCommand};
 
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    nationality: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
-
-
-
-fn main() -> anyhow::Result<()>{
+fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
-    
+
     match opts.cmd {
         SubCommand::Csv(opts) => {
-            let input_file = File::open(opts.input)?;
-            let mut rdr = Reader::from_reader(input_file);
-            let mut ret = Vec::with_capacity(128);
-            for result in rdr.deserialize() {
-                let record: Player = result?;
-                println!("{:?}", record);
-                ret.push(record);
-            }
-
-            // write to output file as json
-            let output_file = File::create(opts.output)?;serde_json::to_writer_pretty(output_file, &ret)?;
-
-        }
+            process_csv(&opts.input, &opts.output)?
+        },
     }
 
     Ok(())
 }
-
-
