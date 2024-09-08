2c03c0480b66a423df913146206eed545a8465a4
Vincent_Li <q315744068@gmail.com>
2024-09-08 12:07:53 +0800

    add file

---
+++ src/process/gen_pass.rs
@@ -0,0 +1,46 @@
+use rand::{seq::SliceRandom, thread_rng};
+use zxcvbn::zxcvbn;
+
+pub fn process_genpass(
+    uppercase: bool,
+    lowercase: bool,
+    number: bool,
+    symbol: bool,
+    length: usize,
+) -> anyhow::Result<()> {
+    let mut rng = thread_rng();
+    let mut password = Vec::with_capacity(length);
+    let mut chars = Vec::new();
+
+    if uppercase {
+        chars.extend('A'..='Z');
+    }
+
+    if lowercase {
+        chars.extend('a'..='z');
+    }
+
+    if number {
+        chars.extend('0'..='9');
+    }
+
+    if symbol {
+        chars.extend("!@#$%^&*_".chars());
+    }
+
+    for _ in 0..length {
+        let random_char = chars.choose(&mut rng).unwrap();
+        password.push(*random_char as u8);
+    }
+
+    password.shuffle(&mut rng);
+
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
+
+    // output password strength in stderr
+    let result = zxcvbn(&password, &[]);
+    eprintln!("Password strength: {}", result.score());
+
+    Ok(())
+}
